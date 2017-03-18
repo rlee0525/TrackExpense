1f96ab143bdfce617645576fd6c5c13d3dd0245d
Raymond Lee <raylee0525@gmail.com>
2017-03-17 20:03:38 -0700

    Implement regular users - table, MVC, redux cycle

--- frontend/track_expense.jsx
+++ frontend/track_expense.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
+  let store;
+  if (window.currentUser) {
+    const preloadedState = { session: { currentUser: window.currentUser } };
+    store = configureStore(preloadedState);
+  } else {
+    store = configureStore();
+  }
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Welcome to TrackExpense!</h1>, root);
+  window.store = store;
 });
